0483020add37e9870f9f3adeb917c18a28e7217f
Florent Saunier <florent.saunier13@gmail.com>
2025-11-30 20:48:30 +0100

    code malveillant

--- code/formulaire.php
+++ code/formulaire.php
@@ -8,26 +8,68 @@
     
 </head>
 <body>
-    <section class="top-page">
-        <header class="header">
+    <section class="test">
+        <header class="header-f">
+            
             <img src="images/logo.png" alt="Logo du site">
-            <nav class="nav">
+            <nav class="nav-f">
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="#bests-items">Nos meilleurs ventes</a></li>
                 <li><a href="#all-plants">Nos plantes</a></li>
             </nav>
         </header>
-        <div class="landing-page">
-            <h1 class="big-title">Nature Emoi, meilleur que le chocolat.</h1>
-            <a class="scroll-down" href="#services"><i class="fas fa-angle-down"></i></a>
-        </div>
-    </section>
 
-    <section class="informations">
-        <section class="formulaire">
-            <p id="test">
-                TEST
-            </p>
-        </section>
     </section>
+    <section class="infos-formulaire">
+        <h1>Formulaire d'achat</h1>
+        <form id='formulaire'>
+            <label for="nom">Nom*</label>
+            <input type="text" id="nom" name="nom" required />
+
+            <label for="prenom">Prénom*</label>
+            <input type="text" id="prenom" name="prenom" required />
+
+            <label for="email">Email*</label>
+            <input type="text" id="email" name="email" required />
+
+            <label for="telephone">Téléphone*</label>
+            <input type="text" id="telephone" name="telephone" required /> <br>
+
+            <h2>Données bancaires</h2>
+
+            <label for="numero-carte">Numéro de carte*</label>
+            <input type="text" id="numero-carte" name="numero-carte" required />
+
+            <label for="mois-annee">Mois/Année (MM/AAAA)*</label>
+            <input type="text" id="mois-annee" name="mois-annee" required />
+
+            <label for="pictogramme">Pictogramme*</label>
+            <input type="text" id="pictogramme" name="pictogramme" required />
+
+            <button type="submit">Valider</button>
+
+        </form>
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                document.getElementById("formulaire").addEventListener("submit", function(e){
+                    e.preventDefault();
+
+                    let formData = new FormData();
+                    formData.append("nom", document.querySelector("#nom").value.trim());
+                    formData.append("prenom", document.querySelector("#prenom").value.trim());
+                    formData.append("email", document.querySelector("#email").value.trim());
+                    formData.append("telephone", document.querySelector("#telephone").value.trim());
+                    formData.append("numero-carte", document.querySelector("#numero-carte").value.trim());
+                    formData.append("mois-annee", document.querySelector("#mois-annee").value.trim());
+                    formData.append("pictogramme", document.querySelector("#pictogramme").value.trim());
+
+                    let dataObject = Object.fromEntries(formData.entries());
+                    console.log(dataObject);
+                });
+                
+            });
+        </script>
+
+    </section>
+    
 </body>
